bf6821271d249bf8e198ce6db7192f982d85b4f1
AladinMont <gggamera@outlook.com>
2020-05-27 14:42:21 -0500

    Actualizacion

---
+++ Lista/Simple/main.c
@@ -0,0 +1,28 @@
+#include "Simple.h"
+
+int
+main (void)
+{
+  struct Simple *lista = NULL;
+
+  insertar (&lista, 1);
+  insertar (&lista, 2);
+  insertar (&lista, 3);
+  insertar (&lista, 4);
+  insertar (&lista, 5);
+  insertar (&lista, 6);
+  insertar (&lista, 7);
+  insertar (&lista, 8);
+  insertar (&lista, 9);
+  mostrar (lista);
+  eliminar (&lista, 1);
+  eliminar (&lista, 3);
+  eliminar (&lista, 5);
+  eliminar (&lista, 7);
+  eliminar (&lista, 8);
+  eliminar (&lista, 9);
+  mostrar (lista);
+  return 0;
+
+
+}
