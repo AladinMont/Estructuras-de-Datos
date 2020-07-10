9a12ba7a1823a5d696fecaa96045f2333ddbcf1a
AladinMont <gggamera@outlook.com>
2020-07-09 21:29:12 -0500

    Pila estatica

---
+++ Pilas/Dinamica/main.c
@@ -0,0 +1,11 @@
+#include "PilaE.h"
+
+int main (void){
+    struct pila *cabeza=(struct pila *)malloc(sizeof(struct pila));
+    cabeza->tope=-1;    
+
+    push(&cabeza,1);
+    push(&cabeza,2);
+    push(&cabeza,3);
+    desplegar(cabeza);
+}
